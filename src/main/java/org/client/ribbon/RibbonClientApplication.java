b0047ea20b2befc80179b370425d33eec94857bd
Sathish Kumar Parthasarathy <p.sathish.cs@gmail.com>
2017-01-22 08:21:41 -0600

    adding EnableCircuitBreaker.

--- src/main/java/org/client/ribbon/RibbonClientApplication.java
+++ src/main/java/org/client/ribbon/RibbonClientApplication.java
@@ -2,9 +2,11 @@ package org.client.ribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 @EnableHystrixDashboard
 public class RibbonClientApplication {
 	public static void main(String[] args){
